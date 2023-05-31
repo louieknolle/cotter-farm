4ccc5f981bdcf174a5e9eab97ba6f7bfaf22b1be
Louie Knolle <knollelw@gmail.com>
2023-05-30 21:14:59 -0400

    conditionally hide/show menu w/ navlinks and menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './media/logo.png'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+  const [isMenuHidden, setMenuHidden] = useState(true)
+
+  const handleClick = () => {
+    if (window.innerWidth < 768) {
+      setMenuHidden(!isMenuHidden)
+    }
+  }
+
   return (
     <div className="flex flex-wrap justify-around w-screen h-40 text-white bg-slate-600 md:flex-nowrap md:h-48">
       {/* <!-- LOGO --> */}
@@ -14,7 +22,10 @@ const Header = () => {
         />
       </NavLink>
       {/* <!-- MenuButton --> */}
-      <button className="z-30 flex items-center justify-end flex-grow pr-3 focus:outline-none md:hidden">
+      <button
+        onClick={handleClick}
+        className="z-30 flex items-center justify-end flex-grow pr-3 focus:outline-none md:hidden"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="w-8 h-8"
@@ -32,27 +43,35 @@ const Header = () => {
       </button>
 
       {/* <!-- Menu --> */}
-      <div className="hidden md:flex md:w-3/4">
+      <div
+        className={`navlink-container ${
+          isMenuHidden ? 'hidden' : ''
+        } md:flex md:w-3/4`}
+      >
         <div className="menu flex flex-col bg-slate-500 items-stretch w-screen text-xl text-center transform md:flex-row md:translate-y-0 md:space-x-5 md:items-center md:justify-end md:pr-10 md:bg-slate-600">
           <NavLink
+            onClick={handleClick}
             to="/"
             className="h-10 leading-10 border-b-2 border-dotted md:border-none block"
           >
             About Us
           </NavLink>
           <NavLink
+            onClick={handleClick}
             to="csa"
             className="h-10 leading-10 border-b-2 border-dotted md:border-none block"
           >
             CSA Info
           </NavLink>
           <NavLink
+            onClick={handleClick}
             to="faqs"
             className="h-10 leading-10 border-b-2 border-dotted md:border-none block"
           >
             FAQS
           </NavLink>
           <NavLink
+            onClick={handleClick}
             to="our-pork"
             className="h-10 leading-10 border-b-2 border-dotted md:border-none block"
           >
@@ -65,32 +84,3 @@ const Header = () => {
 }
 
 export default Header
-
-{
-  /* <NavLink
-            to="/"
-            className="text-gray-800 text-lg font-semibold hover:underline cursor-pointer"
-          >
-            About Us
-          </NavLink>
-
-          <NavLink
-            to="csa"
-            className="text-gray-800 text-lg font-semibold hover:underline cursor-pointer"
-          >
-            CSA Info
-          </NavLink>
-          <NavLink
-            to="faqs"
-            className="text-gray-800 text-lg font-semibold hover:underline cursor-pointer"
-          >
-            FAQs
-          </NavLink>
-          <NavLink
-            to="our-pork"
-            className="text-gray-800 text-lg font-semibold hover:underline cursor-pointer"
-          >
-            Our Pork
-          </NavLink>
-        </div> */
-}
